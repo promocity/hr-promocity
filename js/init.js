67963cf4980c47b52c601d20597ab9268a24ac03
Илья <verst.promocity.ekb@yandex.ru>
2015-11-05 14:33:24 +0500

    perfect pixel

---
+++ js/init.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.slider2').mobilyslider({
+		transition: 'vertical',
+		animationSpeed: 500,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnHover: true,
+		bullets: false
+	});
+});
